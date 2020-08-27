7e5a581269ccba0eb1ae2416f5bcad5e8c339ef1
Eiqli <15053205952@163.com>
2020-08-27 13:44:57 +0800

    初始化项目

---
+++ src/views/newPages/newPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="new">新增页面</div>
+</template>
+
+<style lang="less">
+@import '~ant-design-vue/lib/style/themes/default.less';
+
+  .new {
+    background-color: red;
+    font-size: 20px
+  }
+</style>
